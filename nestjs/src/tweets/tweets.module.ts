bf3deb18f18ac207f3ec8ec0d1531f5bc7c38152
omateustavares <mateustm17@gmail.com>
2022-07-06 23:43:10 -0300

    chore: adding config docker compose e new entities

--- nestjs/src/tweets/tweets.module.ts
+++ nestjs/src/tweets/tweets.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
+import { Tweet, TweetSchema } from './schemas/tweet.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Tweet.name, schema: TweetSchema }]),
+  ],
   controllers: [TweetsController],
   providers: [TweetsService],
 })
