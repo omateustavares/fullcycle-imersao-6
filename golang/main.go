7432d7de24b617f8c5b6ced592f3b027d59f0aa2
Mateus Tavares <mateustm17@gmail.com>
2022-07-29 20:25:42 -0300

    chore: without secret

--- golang/main.go
+++ golang/main.go
@@ -4,42 +4,46 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"github.com/omateustavares/fullcycle-imersao-6/email"
-	"github.com/omateustavares/fullcycle-imersao-6/kafka"
+	"github.com/codeedu/imersao6-email/email"
+	"github.com/codeedu/imersao6-email/kafka"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	gomail "gopkg.in/mail.v2"
+	"os"
+	"strconv"
 )
 
 func main() {
 	var emailCh = make(chan email.Email)
 	var msgChan = make(chan *ckafka.Message)
 
-	// port, _ := strconv.Atoi(os.Getenv("587"))
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
 	
 	d := gomail.NewDialer(
-		"smtp.mailgun.org",
-		587,
-		"teste@sandbox0270ddb825c64e9bbf11ad65b642d419.mailgun.org",
-		"82fa5f618f3eba099810b8cfcd5e93e7-787e6567-1ac85fb5",
+		os.Getenv("MAIL_HOST"),
+		port,
+		os.Getenv("MAIL_USER"),
+		os.Getenv("MAIL_PASSWORD"),
 	)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
 	es := email.NewMailSender()
-	es.From = "mateustm17@gmail.com"
+	es.From = os.Getenv("MAIL_FROM")
 	es.Dialer = d
 
-	go es.Send(emailCh)
+	for i := 1; i <= 2; i++ {
+		go es.Send(emailCh, i)
+	}
 
 	configMapConsumer := &ckafka.ConfigMap{
-		"bootstrap.servers": "kafka:9094",
-		// "bootstrap.servers": os.Getenv("BOOTSTRAP_SERVERS"),
-		// "security.protocol": os.Getenv("SECURITY_PROTOCOL"),
-		// "sasl.mechanisms":   os.Getenv("SASL_MECHANISMS"),
-		// "sasl.username":     os.Getenv("SASL_USERNAME"),
-		// "sasl.password":     os.Getenv("SASL_PASSWORD"),
-		"client.id": "emailapp",
-		"group.id":  "emailapp",
-		// "session.timeout.ms": 45000,
+		//"bootstrap.servers": "kafka:9094",
+		"bootstrap.servers": os.Getenv("BOOTSTRAP_SERVERS"),
+		"security.protocol": os.Getenv("SECURITY_PROTOCOL"),
+		"sasl.mechanisms":   os.Getenv("SASL_MECHANISMS"),
+		"sasl.username":     os.Getenv("SASL_USERNAME"),
+		"sasl.password":     os.Getenv("SASL_PASSWORD"),
+		"client.id": "goapp",
+		"group.id":  "goapp1",
+		"session.timeout.ms": 45000,
 	}
 	topics := []string{"emails"}
 	consumer := kafka.NewConsumer(configMapConsumer, topics)
@@ -49,7 +53,6 @@ func main() {
 	for msg := range msgChan {
 		var input email.Email
 		json.Unmarshal(msg.Value, &input)
-		fmt.Println("Recebendo mensagem")
 		emailCh <- input
 	}
 }
